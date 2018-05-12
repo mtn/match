c685fd2be1cedfa4899790de6110bbaf5eea6fd2
Michael Noronha <michaeltnoronha@gmail.com>
2018-05-12 17:58:11 -0500

    Add search within strings

--- match.rb
+++ match.rb
@@ -22,11 +22,31 @@ def match(pattern, text)
   # If the pattern is exhausted, the text matched
   if pattern == ""
     return true
+  # We can match the end of strings with `$`
+  elsif pattern == "$" && text == ""
+    return true
   end
 
   return match_one(pattern[0], text[0]) &&
          match(pattern[1..-1], text[1..-1])
 end
 
+# The `^` character restricts matching to the beginning of strings
+def search(pattern, text)
+  if pattern[0] == "^"
+    return match(patter[1..-1], text)
+  else
+    text.each_char.with_index { |_, i|
+      if match(pattern, text[i..-1])
+        return true
+      end
+    }
+
+    false
+  end
+end
+
+
 puts match_one(".", "a")
 puts match("abc", "ab")
+puts search("abc", "dabc")
